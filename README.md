652e32b59a03d346d02af18df9d7496b86c48694
bob <bob@>
2022-03-17 14:25:08 -0400

    Hey can someone fix this lmao

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+
 Haha im a programmer now.
 A funny haxeflixel thing that I decided to make since I needed to learn a coding language. But like good.
+I'm making a platformer where the enemies are godot logos and they are trying to beatup haxefixel logo.
+Why did i want to learn haxeflixel instead of godot
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,42 +3,99 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.util.FlxColor;
+import js.lib.intl.Collator.Collation;
 
 class PlayState extends FlxState
 {
-	public static var Player:FlxSprite;
-
-	var sprite:FlxSprite;
+	var scaleFactor = 0.1;
+	var Player:FlxSprite;
+	var Ground:FlxSprite;
 
 	override function create()
 	{
 		super.create();
-		sprite = new FlxSprite("assets/holyshititshaxe.png");
-		sprite.x = FlxG.width / 2 - sprite.width / 2;
-		sprite.acceleration.y = 600;
-		sprite.maxVelocity.y = 200;
+		Player = new FlxSprite("assets/holyshititshaxe.png");
+		Player.x = FlxG.width / 2 - Player.width / 2;
+		Player.acceleration.y = 0;
+		Player.maxVelocity.y = 200;
 
-		add(sprite);
+		Ground = new FlxSprite(AssetPaths.ground__png);
+		// Pixel Perfect rendering
+		Ground.pixelPerfectRender = true;
+		// Pixel Perfect positioning
+		Ground.pixelPerfectPosition = true;
+		Ground.x = FlxG.width / 2 - FlxG.width / 2;
+		Ground.y = 300;
+
+		Player.velocity.y = 150;
 		Music.playMusic();
-		add(new Player());
-		add(new Ground());
+		add(Player);
+		add(Ground);
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
 
-		if (sprite.velocity.y > 0 && sprite.y >= FlxG.height + 8)
+		// Gravity Shit
+		if (Player.velocity.y > 0 && Player.y >= FlxG.height + 8)
 		{
-			sprite.y = FlxG.height + 8;
-			sprite.acceleration.y = 0;
-			sprite.velocity.y = -100;
+			Player.y = FlxG.height + 8;
+			Player.acceleration.y = 0;
+			Player.velocity.y = -100;
 		}
-		else if (sprite.velocity.y < 0 && sprite.y <= 0)
+		else if (Player.velocity.y < 0 && Player.y <= 0)
 		{
-			sprite.y = 0;
-			sprite.velocity.y = 0;
-			sprite.acceleration.y = 800;
+			Player.y = 0;
+			Player.velocity.y = 0;
+			Player.acceleration.y = 800;
 		}
+
+		{
+			super.update(elapsed);
+
+			FlxG.collide(Player, Ground, Collation);
+
+			if (Player.x <= 0)
+				Player.velocity.x *= -1;
+			if (Ground.x >= FlxG.width - Ground.width)
+				Ground.velocity.x *= -1;
+		}
+
+		function collision(Player:FlxSprite, Ground:FlxSprite)
+		{
+			Player.velocity.x = -150;
+			Ground.velocity.x = 150;
+		}
+
+		// Keyboard Movement
+		if (FlxG.keys.pressed.A || FlxG.keys.pressed.LEFT)
+			Player.x--;
+		if (FlxG.keys.pressed.D || FlxG.keys.pressed.RIGHT)
+			Player.x++;
+		if (FlxG.keys.pressed.W || FlxG.keys.pressed.UP)
+			Player.y--;
+		if (FlxG.keys.pressed.S || FlxG.keys.pressed.DOWN)
+			Player.y++;
+		if (FlxG.keys.justPressed.SPACE)
+		{
+			Player.y = FlxG.height / 2 - Player.height / 2;
+			Player.x = FlxG.width / 2 - Player.width / 2;
+		}
+
+		// Mouse Movement
+		if (FlxG.mouse.pressed)
+			Player.setPosition(FlxG.mouse.getPosition().x - Player.width / 2, FlxG.mouse.getPosition().y - Player.height / 2);
+
+		if (FlxG.mouse.justReleasedRight)
+		{
+			Player.y = FlxG.height / 2 - Player.height / 2;
+			Player.x = FlxG.width / 2 - Player.width / 2;
+			Player.scale.set(1, 1);
+		}
+
+		if (FlxG.mouse.wheel != 0)
+			Player.scale.add(FlxG.mouse.wheel * 0.1, FlxG.mouse.wheel * 0.1);
 	}
 }
